70e9d427240be801ba3fc7e891de312c70bad592
Sweta <44766850+swetanakadi@users.noreply.github.com>
2022-07-05 20:04:38 +0530

    Update settings.py

--- BulletJournal/settings.py
+++ BulletJournal/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-==8t=rwi=y$yle2tgbz3==id2g71_g9k2vjt^4$bi@*4tj3l4='
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
